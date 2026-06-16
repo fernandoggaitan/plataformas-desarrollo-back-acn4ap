8ff066f4acdef057f8235fa62550ac3919f83f2d
fernandoggaitan <fernandoggaitan@gmail.com>
2026-06-16 19:58:24 -0300

    accessToken

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,5 +1,8 @@
 const usuarioModel = require("../models/usuarioModel");
 
+//Jason Web Token
+const jwt = require('jsonwebtoken');
+
 exports.register = async(req, res) => {
     //Verifica que haya un body.
     if (!req.body) {
@@ -42,16 +45,36 @@ exports.login = async(req, res) => {
         if(usuario == null){
             res.status(401).json({ success: false, message: 'Credenciales incorrectas' });
         }else{
+
+            //Información que se va a hashear.
+            const playload = { 
+                ID: usuario.id, 
+                nombre: usuario.nombre, 
+                is_admin: (usuario.is_admin == '1') 
+            };
+
+            //Access token.
+            const accessToken = jwt.sign(
+                playload, 
+                process.env.JWT_ACCESS_TOKEN_SECRET, 
+                { expiresIn: '15m' }
+            );
+
             res.json({
                 success: true,
                 message: 'Inicio de sesión exitoso',
-                nombre: usuario.nombre
+                nombre: usuario.nombre,
+                accessToken
             });
         }
     }catch(error){
         console.log(error);
         res.status(500).json({ success: false, message: 'Error al intentar iniciar sesión' });
     }
+}
+
+exports.welcome = (req, res) => {
+    res.json({ success: true, message: 'Bienvenida/o ' + req.user.nombre });
 }
 
 // Función de validación de usuario.
--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -4,7 +4,11 @@ const router = express.Router();
 //Controlador
 const usuarioController = require("../controllers/usuarioController");
 
+//Middleware para verificar que el usuario inició sesión.
+const { requireAuth } = require("../middlewares/auth");
+
 router.post("/register", usuarioController.register);
 router.post("/login", usuarioController.login);
+router.get("/welcome", requireAuth, usuarioController.welcome);
 
 module.exports = router;
