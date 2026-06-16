3723e314e8678013b42bc5dc85e5587ee6ca24d5
fernandoggaitan <fernandoggaitan@gmail.com>
2026-06-16 20:41:26 -0300

    refreshToken

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -60,16 +60,47 @@ exports.login = async(req, res) => {
                 { expiresIn: '15m' }
             );
 
+            //Refresh token
+            const refreshToken = jwt.sign(
+                playload, 
+                process.env.JWT_REFRESH_TOKEN_SECRET, 
+                { expiresIn: '7d' }
+            );
+
             res.json({
                 success: true,
                 message: 'Inicio de sesión exitoso',
                 nombre: usuario.nombre,
-                accessToken
+                accessToken,
+                refreshToken
             });
         }
     }catch(error){
         console.log(error);
         res.status(500).json({ success: false, message: 'Error al intentar iniciar sesión' });
+    }
+}
+
+exports.refreshToken = (req, res) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({ success: false, message: 'Token de autenticación no proporcionado' });
+    }
+    // El valor del encabezado de autorización debe tener el formato "Bearer tu_token_jwt_aqui"
+    const [bearer, token] = authHeader.split(' ');
+    if (bearer !== 'Bearer' || !token) {
+        return res.status(401).json({ success: false, message: 'Formato de token no válido' });
+    }
+    try{
+        const usuario = jwt.verify(token, process.env.JWT_REFRESH_TOKEN_SECRET);
+        //Información que se va a hashear.
+        const playload = { ID: usuario.ID, nombre: usuario.nombre, is_admin: usuario.is_admin };
+        // Ahora, user contiene información del usuario. Puedes generar un nuevo accessToken.
+        const newAccessToken = jwt.sign(playload, process.env.JWT_ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+        // Devolver el nuevo accessToken al cliente
+        res.json({ success: true, accessToken: newAccessToken });
+    }catch(error){
+        return res.status(401).json({ success: false, message: 'Token de autenticación inválido' });
     }
 }
 
--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -8,7 +8,14 @@ const usuarioController = require("../controllers/usuarioController");
 const { requireAuth } = require("../middlewares/auth");
 
 router.post("/register", usuarioController.register);
+
+//Se loguea.
 router.post("/login", usuarioController.login);
+
+//El usuario debe acceder a la siguiente ruta ya logueado.
 router.get("/welcome", requireAuth, usuarioController.welcome);
+
+//Genera un nuevo acccess token.
+router.get('/refresh-token', usuarioController.refreshToken);
 
 module.exports = router;
